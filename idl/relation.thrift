bff345eab7e13092629500b8e9aeec7e9a662082
zxz <xuanweiace@163.com>
2023-01-23 20:25:53 +0800

    add idl

--- idl/relation.thrift
+++ idl/relation.thrift
@@ -12,3 +12,51 @@ struct RelationActionRequest {
 struct RelationActionResponse {
     1: required BaseResponse base_resp
 }
+
+
+struct User{
+    1:required  i64     id
+    2:required  string  name
+    3:optional  i64     follow_count
+    4:optional  i64     follower_count
+    5:required  bool    is_follow
+}
+
+struct GetFollowListRequest{
+    1:required  i64 user_id
+    2:required  string token
+}
+
+struct GetFollowListResponse{
+    1:BaseResponse base_resp
+    2:list<User> user_list
+}
+
+struct GetFollowerListRequest{
+    1:required  i64 user_id
+    2:required  string token
+}
+
+struct GetFollowerListResponse{
+    1:BaseResponse base_resp
+    2:list<User> user_list
+}
+
+struct GetFriendListRequest{
+    1:required  i64 user_id
+    2:required  string token
+}
+
+struct GetFriendListResponse{
+    1:BaseResponse base_resp
+    2:list<User> user_list
+}
+
+service RelationService{
+    RelationActionResponse   RelationAction(1:RelationActionRequest    req)
+    GetFollowListResponse  GetFollowList(1:GetFollowListRequest  req)
+    GetFollowerListResponse  GetFollowerList(1:GetFollowerListRequest  req)
+    GetFriendListResponse    GetFriendList(1:GetFriendListRequest req)
+}
+
+
