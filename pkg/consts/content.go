6f04afd18880e10569da589aa6f201537636355e
doingc <c1356@qq.com>
2023-01-27 23:33:07 +0800

    加入viper

--- pkg/consts/content.go
+++ pkg/consts/content.go
@@ -1,4 +1,4 @@
-package main
+package consts
 
 import (
 	"fmt"
@@ -20,10 +20,25 @@ var (
 func readInfo() {
 	work, err := os.Getwd()
 	if err != nil {
-		str := ""
+		str := "获取错误路径出现问题"
 		errnos.Wrap(err, str)
 	}
-	viper.SetConfigName("info")
-	viper.SetConfigFile("yml")
 	fmt.Println(work)
+	viper.SetConfigName("config")
+	viper.SetConfigType("yml")
+	viper.AddConfigPath(work)
+	err = viper.ReadInConfig()
+	if err != nil {
+		errnos.Wrap(err, "读取配置文件问题")
+	}
+	account := viper.GetString("DSN.account")
+	password := viper.GetString("DSN.password")
+	dbname := viper.GetString("DSN.Dbname")
+	host := viper.GetString("DSN.host")
+	//读取正式内容
+	MySQLDefaultDSN = account + ":" + password + "@tcp(localhost:" + host + ")/" + dbname + "?charset=utf8&parseTime=True&loc=Local"
+	RelationTableName = viper.GetString("Table.relation")
+	RelationServiceName = viper.GetString("ServiceName")
+	EtcdAddress = viper.GetString("etcd.address")
+
 }
--- pkg/consts/main.go
+++ pkg/consts/main.go
@@ -1,4 +1,4 @@
-package main
+package consts
 
 func main() {
 	readInfo()
