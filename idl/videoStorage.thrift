c597b7a429b427cd6061587d215befe0f059c792
bogo_y <boostgoing@gmail.com>
2023-01-27 21:13:09 +0800

    changeidl

--- idl/videoStorage.thrift
+++ idl/videoStorage.thrift
@@ -1,15 +1,26 @@
 namespace go videoStorage
 
 struct VideoURLResponse {
-    1: required string playURL;
-    2: required string coverURL;
-    3: required i64 authorID;
+    1: required string play_url;
+    2: required string cover_url;
+    3: required i64 author_id;
 }
 
 struct VideoURLRequest {
-    1: required i64 videoID;
+    1: required i64 video_id;
 }
 
-service VideoService {
+struct VideoUploadRequest {
+    1:required binary video_data;
+    2:required i64 video_id;
+    3:required binary cover_data;
+}
+struct VideoUploadResponse {
+    1:required bool result;
+    2: string message;
+}
+
+service VideoStorageService {
     VideoURLResponse queryVideoURL(1: VideoURLRequest req)
+    VideoUploadResponse uploadVideoToDB(1: VideoUploadRequest req)
 }
