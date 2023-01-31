2ca88bc1d2d5a9083de96a3056b9290af61be3fa
doingc <c1356@qq.com>
2023-01-31 19:19:12 +0800

    test

--- pkg/middleware/mq.go
+++ pkg/middleware/mq.go
@@ -1,4 +1,7 @@
-package middleware
+package main
 
-//常见mq ActiveMQ、RabbitMQ、RocketMQ、Kafka（维护成本高）
-//rabbitmq 异步场景 将信息写入数据库+发送成功邮件 解耦 削峰
+// 常见mq ActiveMQ、RabbitMQ、RocketMQ、Kafka（维护成本高）
+// rabbitmq 异步场景 将信息写入数据库+发送成功邮件 解耦 削峰
+func main() {
+
+}
