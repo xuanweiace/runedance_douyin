f8eeee2ac2ba84422cdea9d2c5a765b648bb4579
wechan <1291801454@qq.com>
2023-02-19 20:08:45 +0800

    redis cache user

--- cmd/user/dal/init.go
+++ cmd/user/dal/init.go
@@ -2,11 +2,13 @@ package dal
 
 import (
 	"runedance_douyin/cmd/user/dal/db_mysql"
+	"runedance_douyin/cmd/user/dal/db_redis"
 	"runedance_douyin/cmd/user/rpc"
 )
 
 func Init() {
 	rpc.Init()
 	db_mysql.MySQLInit()
+	db_redis.InitRedis("localhost:6379", "")
 
 }
